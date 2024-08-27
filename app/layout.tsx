6e18ef9d636d059f076188888459a624d8fc59e6
muradpm <abdulkadyr0v@yandex.ru>
2024-08-27 23:19:16 +0300

    add flomni favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,10 @@ import "./globals.css";
 import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
-  title: "Flomni AI токен калькулятор",
+  title: "Flomni AI калькулятор",
   description: "Рассчитайте количество токенов и стоимость использования AI моделей",
   icons: {
-    icon: [
-      {
-        url: "/flomni.svg",
-      },
-    ],
+    icon: "/flomni.svg",
   },
 };
 
